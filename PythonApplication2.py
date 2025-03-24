407e2fc9bf9e42fc8fb29df0249397aed68f22f7
opilane <opilane@HP-CZC2349HT9>
2025-03-24 11:51:53 +0200

    mandaadihaldur очищай!

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -3,14 +3,5 @@ sınad=["maja", "auto", "p‰ike", "ıhtu", "ıunad", "kala", "kool", "raamat"]
 salasına=choice(sınad)
 k=len(salasına)
 p=6
-while p>0:
-    print("_ "*k)
-    print(f"On j‰‰nud {p} katset")
-    p-=1
-    sına=input("Sisesta oma variant: ").lower()
-    if len(sına)!=k:
-        print(f"Sına peab olema {k} t‰hte pikk!")
-        continue
-    if sına==salasına:
-        print("Huraa!")
+
       
